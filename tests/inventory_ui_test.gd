c87ac5fee201c49af6d63ec83be0aef82a99b427
Kolt McBride <koltafrickenfer@gmail.com>
2025-08-14 13:53:58 -0700

    Handle inventory toggle via unhandled input

--- tests/inventory_ui_test.gd
+++ tests/inventory_ui_test.gd
@@ -1,25 +1,21 @@
 extends SceneTree
 
+const Inventory = preload("res://inventory.gd")
+
 func _init():
-	var scene = preload("res://terrain_generator.tscn").instantiate()
-	get_root().add_child(scene)
-	await process_frame
-	var ui = scene.get_node("InventoryUI")
-	assert(ui.panel.visible == false)
-	var ev := InputEventAction.new()
-	ev.action = "inventory"
-	ev.pressed = true
-	ui._input(ev)
-	assert(ui.panel.visible == true)
-	var camera = scene.get_node("Player/Camera3D")
-	camera.make_current()
-	var pickup = scene.get_node("WoodPickup")
-	var screen_pos = camera.unproject_position(pickup.global_position)
-	var ic = scene.get_node("InteractionController")
-	ic.handle_mouse_click(screen_pos)
-	await process_frame
-	var inventory = scene.get_node("Inventory")
-	assert(inventory.items.size() == 1)
-	assert(!is_instance_valid(pickup))
-	print("Inventory UI test passed")
-	quit()
+        var inventory = Inventory.new()
+        inventory.player_path = NodePath(".")
+        get_root().add_child(inventory)
+        await process_frame
+        var ui = load("res://inventory_ui.tscn").instantiate()
+        get_root().add_child(ui)
+        await process_frame
+        assert(ui.panel.visible == false)
+        var ev := InputEventAction.new()
+        ev.action = "inventory"
+        ev.pressed = true
+        Input.parse_input_event(ev)
+        await process_frame
+        assert(ui.panel.visible == true)
+        print("Inventory UI test passed")
+        quit()
